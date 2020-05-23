efc2090d430f6ee08c05c142e4ff49df5d26aa70
樋口心 <higuchikokoro@higuchishinnoMacBook-Air.local>
2020-05-23 22:35:25 +0900

    add signup controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
+# Rails.application.routes.draw do
+ # get 'users/new'
+
+#   root 'static_pages#home'
+#   get  'static_pages/home'
+#   get  'static_pages/help'
+#   get  'static_pages/about'
+#   get  'static_pages/contact'
+# end
+
+
 Rails.application.routes.draw do
-  root 'static_pages#hello'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
+  get 'users/new'
+
+  root 'static_pages#home'
+  # When get request send to /help, routes call to help action of StaticPages Controller.
+  get  '/signup', to: 'users#new'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
